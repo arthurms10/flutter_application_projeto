24726180f508521fba7de0c006f961f6bb69a218
Arthur Santos <arthurmsantos10@gmail.com>
2024-04-03 20:41:05 -0300

    sd

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,6 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Navegação',
-
       //Rotas de navegação
       //
       initialRoute: 'login',
